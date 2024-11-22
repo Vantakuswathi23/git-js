c577b1bf47097232a8f92329faca292fa2fca8c4
Vantakuswathi23 <vantakuswathi523@gmail.com>
2024-11-22 14:01:17 +0530

    css var

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,4 @@
 // document.getElementById("demo").innerHTML="hii this is the first javascript code";
-var a=document.getElementById('demo1');
-a.style.backgroundColor('--color-alert','#c49d0a')
+var a = document.getElementById('demo1');
+a.style.backgroundColor = 'var(--color-alert)';
+// document.documentElement.style.setProperty('--color-alert', '#c49d0a');
